f1f4c71f01835271d26105f36f017833323f6aab
Jack Blanc <jackblanc0@proton.me>
2024-05-30 18:19:36 -0400

    update to beta drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,5 @@
 import { type Config } from "drizzle-kit";
 
-// NOTE: Migration fails. The error is:
-// ➜  cloudflare-d1-trpc-api git:(main) ✗ bun run db:migrate
-// $ sst shell drizzle-kit migrate
-// drizzle-kit: v0.21.4
-// drizzle-orm: v0.30.10
-// No config path provided, using default path
-// Reading config file '/Users/jackblanc/projects/cloudflare-d1-trpc-api/drizzle.config.ts'
-//  Invalid input  Either "turso", "libsql", "better-sqlite" are available options for "--driver"
-// ×  exit status 1
-// error: script "db:migrate" exited with code 1
-
 export default {
   schema: "./src/db/schema.ts",
   dialect: "sqlite",
